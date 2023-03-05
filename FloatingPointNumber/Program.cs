cf431b7d7c873829c632460c9f756580c7401950
An Nguyen Le Thien <an@inspectorio.com>
2023-03-05 07:55:16 +0700

    ConvertSingleFloatingPointToDecimal for ideal cases

--- FloatingPointNumber/Program.cs
+++ FloatingPointNumber/Program.cs
@@ -6,8 +6,11 @@ namespace FloatingPointNumber
     {
         static void Main(string[] arg)
         {
+            // 1. Nhập vào 1 số chấm động, xuất ra định dạng nhị phân Single Floating Point tương ứng:
+            //FloatingPointNumberToBinarySingleFloatingPoint.ConvertFloatingPointNumberToBinarySingleFloatingPoint();
 
-            FloatingPointNumberToBinarySingleFloatingPoint.ConvertFloatingPointNumberToBinarySingleFloatingPoint();
+            // 2. Nhập vào 1 dãy bit nhị phân (32 bit) ở dạng Single Floating Point xuát ra giá trị thập phân tương ứng:
+            SingleFloatingPointToDecimal.ConvertSingleFloatingPointToDecimal();
 
         }
     }
---
+++ FloatingPointNumber/SingleFloatingPointToDecimal.cs
@@ -0,0 +1,122 @@
+﻿using System;
+namespace FloatingPointNumber
+{
+	public class SingleFloatingPointToDecimal
+	{
+        public static void ConvertSingleFloatingPointToDecimal()
+        {
+            Console.Write("Input a single precision floating point BINARY: ");
+            string floatingPointBinaryString = Console.ReadLine();
+
+            string signBit = floatingPointBinaryString.Substring(0, 1);
+            string exponentContentPart = floatingPointBinaryString.Substring(1, 8);
+            string significandContentPart = floatingPointBinaryString.Substring(9, 23);
+
+            Console.WriteLine($"signBit: {signBit}");
+            Console.WriteLine($"exponentContentPart: {exponentContentPart}");
+            Console.WriteLine($"significandContentPart: {significandContentPart}");
+
+            string sign = signBit == "1" ? "-" : "+";
+            int exponentValue = ExponentBinToDecAlgorithm( exponentContentPart);
+            string significandContent = RetrieveSignificand(significandContentPart);
+
+            string OneDotForm = $"{sign}1.{significandContent} * 2^{exponentValue}";
+            Console.WriteLine($"The +/- 1.F * 2^E representation for {floatingPointBinaryString} is: {OneDotForm}");
+
+            string originalBinaryFloatingPoint = OriginalBinaryFloatingPoint(exponentValue, significandContent);
+            Console.WriteLine($"The original binary floating point is: {sign}{originalBinaryFloatingPoint}");
+
+            string originalRealNumber = ConvertOriginalBinaryFloatingPointToDecimal(sign, originalBinaryFloatingPoint);
+            Console.WriteLine($"The original real number of single-precision-floating-point {floatingPointBinaryString} is {originalRealNumber}");
+            Console.Read();
+        }
+
+        public static int ExponentBinToDecAlgorithm(string binaryString)
+        {
+            int sum = 0;
+            string[] binaryElements = FloatingPointNumberToBinarySingleFloatingPoint.SplitStringInToArrayOfStrings(binaryString);
+            Array.Reverse(binaryElements);
+            for(int i = 0; i < binaryElements.Length; i++)
+            {
+                if (binaryElements[i] == "1")
+                {
+                    sum += (int)Math.Pow(2, i);
+                }
+            }
+            return sum - 127;
+        }
+
+        public static string RetrieveSignificand(string binSignificandPart)
+        {
+            string[] binSignificandElements = FloatingPointNumberToBinarySingleFloatingPoint.SplitStringInToArrayOfStrings(binSignificandPart);
+            int lastBit1Index = Array.LastIndexOf(binSignificandElements, "1");
+            string significandContent = binSignificandPart.Substring(0, lastBit1Index + 1);
+            return significandContent;
+        }
+
+        public static string OriginalBinaryFloatingPoint( int exponentValue, string significandContent)
+        {
+            string integerBinaryPart = "";
+            string decimalBinaryPart = "";
+            string shiftingContent = "";
+            if(exponentValue < 0)
+            {
+                integerBinaryPart += "0";
+                shiftingContent += "1";
+                shiftingContent.PadLeft(exponentValue, '0');
+                decimalBinaryPart += shiftingContent.Concat(significandContent);
+            }
+
+            if(exponentValue > 0)
+            {
+                integerBinaryPart += "1";
+                shiftingContent += significandContent.Substring(0, exponentValue);
+                integerBinaryPart += shiftingContent;
+                decimalBinaryPart += significandContent.Substring(exponentValue);
+            }
+
+            return $"{integerBinaryPart}.{decimalBinaryPart}";
+        }
+
+        public static string ConvertOriginalBinaryFloatingPointToDecimal(string sign, string originalBinaryFloatingPoint)
+        {
+            string[] originalBinaryFloatingPointElements = originalBinaryFloatingPoint.Split(".");
+            string integerBinaryPart = originalBinaryFloatingPointElements[0];
+            string decimalBinaryPart = originalBinaryFloatingPointElements[1];
+
+            int integerPart = IntegerBinaryPartToInteger(integerBinaryPart);
+            float decimalPart = DecimalBinaryPartToDecimalPart(decimalBinaryPart);
+
+            return $"{sign}{integerPart + decimalPart}";
+        }
+
+        public static int IntegerBinaryPartToInteger(string integerBinPart)
+        {
+            string[] binElements = FloatingPointNumberToBinarySingleFloatingPoint.SplitStringInToArrayOfStrings(integerBinPart);
+            Array.Reverse(binElements);
+            int sum = 0;
+            for(int i = 0; i < binElements.Length; i++)
+            {
+                if (binElements[i] == "1")
+                {
+                    sum += (int)Math.Pow(2, i);
+                }
+            }
+            return sum;
+        }
+        public static float DecimalBinaryPartToDecimalPart(string decimalBinaryPart)
+        {
+            string[] binElements = FloatingPointNumberToBinarySingleFloatingPoint.SplitStringInToArrayOfStrings(decimalBinaryPart);
+            float sum = 0;
+            for(int i = 0; i < binElements.Length; i++)
+            {
+                if (binElements[i] == "1")
+                {
+                    sum += (float)Math.Pow(2, -(i+1));
+                }
+            }
+            return sum;
+        }
+    }
+}
+
